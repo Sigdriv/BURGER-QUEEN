05980566143967f36df0bfe9e7bebffd15592863
Sigurd Drivstuen <sigdriv06@gmail.com>
2023-12-13 15:27:38 +0100

    Refactor user interface and add order functionality

--- BurgerQueen.py
+++ BurgerQueen.py
@@ -10,6 +10,8 @@ def logInnInterface():
     global error
     
     clear_terminal()
+    print()
+    
     print("1. Logg inn")
     print("2. Opprett bruker")
     print("3. Tilbake")
@@ -30,6 +32,7 @@ def logInnInterface():
             signUp()
     
     elif choice == "3":
+        clear_terminal()
         return False
     
     else:
@@ -40,6 +43,7 @@ def logInn():
     global current_user
     global error
     clear_terminal()
+    print()
     
     print("1. Logg inn")
     print("Please enter your username and password")
@@ -62,9 +66,9 @@ def signUp():
     global error
     global info
     clear_terminal()
+    print()
     
     print("2. Opprett bruker")
-    print()
     print("Skriv inn ønsket brukernavn og passord")
     username = input("Brukernavn: ")
     password = input("Passord: ")
@@ -111,17 +115,96 @@ def clear_terminal():
 def connect_database():
     return sql.connect("BurgerQueen.db")
 
+def get_burger_names():
+    conn = connect_database()
+    c = conn.cursor()
+
+    c.execute("SELECT NameID FROM Burgers")
+    burger_names = [row[0] for row in c.fetchall()]
+
+    conn.close()
+    return burger_names
+
+def orderInterface():
+    global error
+    
+    clear_terminal()
+    print("2. Ordre")
+    print()
+    print("1. Vis ordre")
+    print("2. Registrer ordre")
+    print("3. Tilbake")
+    choice = input("Velg en handling: ")
+    
+    if choice == "1":
+        display_user_orders()
+    elif choice == "2":
+        place_order()
+    elif choice == "3":
+        main()
+    else:
+        error = "Ugyldig valg. Prøv igjen."
+        orderInterface()
+
+
+def place_order():
+    global current_user
+    global info
+    clear_terminal()
+    print("2. Registrer ordre")
+    print()
+    
+    burger_names = get_burger_names()
+    
+    print("Available Burgers:")
+    for i, burger_name in enumerate(burger_names, start=1):
+        print(f"{i}. {burger_name}")
+    print()
+        
+    burger_name = input("Enter burger name: ")
+
+    conn = connect_database()
+    c = conn.cursor()
+
+    c.execute("INSERT INTO Orders (Who, What, Produced) VALUES (?, ?, 0)",
+              (current_user, burger_name))
+    
+    conn.commit()
+    conn.close()
+
+    info = f"Order placed successfully for {burger_name}!"
+    main()
+    
+def display_user_orders():
+    global current_user
+
+    conn = connect_database()
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM Orders WHERE Who = ?", (current_user))
+    orders = c.fetchall()
+
+    if not orders:
+        print("No orders found for the current user.")
+    else:
+        print("User Orders:")
+        for order in orders:
+            print(f"OrderID: {order[0]}, Burger: {order[2]}, Produced: {'Yes' if order[4] else 'No'}")
+
+    conn.close()
+
+
 # Hovedfunksjon
 def main():
     global current_user
     global error
     
-    clear_terminal()
 
     while True:
+        clear_terminal()
+        print()
         if current_user is None:
-            print("1. Logg inn")
-            # print("2. Registrer ordre")
+            print("1. Logg inn/opprett bruker")
             print("2. Avslutt")
 
             choice = input("Velg en handling: ")
@@ -129,9 +212,6 @@ def main():
             if choice == "1":
                 
                 logInnInterface()
-
-            # elif choice == "2":
-            #     pass
 
             elif choice == "2":
                 print("Avslutter programmet.")
@@ -143,7 +223,7 @@ def main():
         
         else:
             print("1. Logg ut")
-            print("2. Registrer ordre")
+            print("2. Ordre")
             print("3. Avslutt")
 
             choice = input("Velg en handling: ")
@@ -153,7 +233,7 @@ def main():
                 clear_terminal()
 
             elif choice == "2":
-                pass
+                orderInterface()
 
             elif choice == "3":
                 print("Avslutter programmet.")
