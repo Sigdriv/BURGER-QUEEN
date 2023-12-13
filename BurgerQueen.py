6e8ae25fefa3adb514b2953771b49cd3fc3d634a
Sigurd Drivstuen <sigdriv06@gmail.com>
2023-12-13 11:03:15 +0100

    Refactor login and sign up functions

--- BurgerQueen.py
+++ BurgerQueen.py
@@ -1,88 +1,99 @@
-import sqlite3
+import sqlite3 as sql
+import os
+import asyncio
 
-username = ""
+current_user = None
 
-def loginnInterface():
+def logInnInterface():
     
-    emptyLinesUpper()
+    clear_terminal()
     print("1. Logg inn")
     print("2. Opprett bruker")
+    print("3. Tilbake")
     choice = input("Velg en handling: ")
-    emptyLinesUpper()
     
     if choice == "1": 
-        if loggInn():
+        if logInn():
             pass
         else:
-            loggInn()
+            logInnInterface()
 
     elif choice == "2":
         if signUp():
             pass
         else:
             signUp()
-
+    
+    elif choice == "3":
+        return False
     
     else:
         print("Ugyldig valg. Prøv igjen.")
         return False
     
-def loggInn():
-        print("Please enter your username and password")
-        username = input("Username: ")
-        password = input("Password: ")
-        emptyLinesUpper()
-        conn = connect_database()
-        c = conn.cursor()
-        c.execute("SELECT * FROM User WHERE UsernameID = ? AND Password = ?", (username, password))
-        if c.fetchone() is not None:
-            print("Welcome " + username)
-            return True, username
-        else:
-            print("Ugyldig brukernavn eller passord")
-            print("Venligst prøv igjen")
-            return False
+def logInn():
+    global current_user
+    clear_terminal()
+    
+    print("1. Logg inn")
+    print("Please enter your username and password")
+    print()
+    username = input("Username: ")
+    password = input("Password: ")
 
-def signUp():
-        print("Skriv inn ønsket brukernavn og passord")
-        username = input("Brukernavn: ")
-        password = input("Passord: ")
-        conn = connect_database()
-        c = conn.cursor()
-        c.execute("SELECT * FROM User WHERE UsernameID = ? AND Password = ?", (username, password))
-        if c.fetchone() is not None:
-            print("Brukernavn eksisterer allerede, vennligst velg et annet")
-            return False
-        else:
-            c.execute("INSERT INTO User VALUES (?, ?, 0)", (username, password))
-            conn.commit()
-            conn.close()
-            print(username + " created")
-            return True
+    conn = connect_database()
+    c = conn.cursor()
+    c.execute("SELECT * FROM User WHERE UsernameID = ? AND Password = ?", (username, password))
+    if c.fetchone() is not None:
+        print("Welcome " + username)
+        current_user = username
+        return True
+    else:
+        print("Ugyldig brukernavn eller passord")
+        print("Venligst prøv igjen")
+        return False
 
+async def signUp():
+    global current_user
+    clear_terminal()
+    
+    print("2. Opprett bruker")
+    print()
+    print("Skriv inn ønsket brukernavn og passord")
+    username = input("Brukernavn: ")
+    password = input("Passord: ")
+    conn = connect_database()
+    c = conn.cursor()
+    c.execute("SELECT * FROM User WHERE UsernameID = ? AND Password = ?", (username, password))
+    if c.fetchone() is not None:
+        print("Brukernavn eksisterer allerede, vennligst velg et annet")
+        await asyncio.sleep(5)
+        return False
+    else:
+        c.execute("INSERT INTO User VALUES (?, ?, 0)", (username, password))
+        conn.commit()
+        conn.close()
+        print(username + " er nå registrert og er logget inn")
+        current_user = username
+        return True
+    
 
-def emptyLinesUpper(count = 2):
-    print('---------------')
-    for i in range(count):
-        print("")
-        
-def emptyLines(count = 1):
-    for i in range(count):
-        print("")
+def clear_terminal():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
 def connect_database():
-    return sqlite3.connect("BurgerQueen.db")
+    return sql.connect("BurgerQueen.db")
 
 # Hovedfunksjon
 def main():
     
-    emptyLines(2)
-    print('------------------------')
-    print("Welcome to Burger Queen")
-    print('------------------------')
+    clear_terminal()
+    print("Velkommen til Burger Queen")
 
     while True:
-        emptyLines(2)
         print("1. Logg inn")
         print("2. Registrer ordre")
         print("3. Avslutt")
@@ -91,13 +102,7 @@ def main():
 
         if choice == "1":
             
-            loginnInterface()
-            
-            if True:
-                pass
-            else:
-                pass
-                
+            logInnInterface()
 
         elif choice == "2":
             pass
